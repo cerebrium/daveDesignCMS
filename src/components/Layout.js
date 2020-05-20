9c62a46542b5035fc8026fe13f892e03b5d1bdeb
cerebrium <nicholas.m.shankland@gmail.com>
2020-05-20 13:48:52 -0700

    changed pic

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -46,7 +46,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${withPrefix('/')}img/og-image.jpg`}
+          content={`${withPrefix('/')}img/topLogo.png`}
         />
       </Helmet>
       <Navbar />
